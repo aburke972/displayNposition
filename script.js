096a8fe3e0b03ee04fb1960bafc686a2b72f01ac
vubuntu20 <selo@vubuntu20.localdomain>
2020-07-18 21:30:08 +0200

    initialize variables n events. Function to be done

---
+++ script.js
@@ -0,0 +1,14 @@
+const buttonInLine = document.getElementById("button-inline")
+const buttonBlock= document.getElementById("button-block")
+const divLeft = document.getElementById("left-container")
+const divCentral = document.getElementById("central-container")
+
+buttonInLine.addEventListener("click",displayInLine)
+buttonBlock.addEventListener("click",displayBlock)
+
+function displayInLine(){}
+function displayBlock(){}
+//To do: Find a way to modify a css element properties
+
+
+//To do : 
