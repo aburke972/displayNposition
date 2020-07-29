59c3649c009d49756567b015c64004406b617622
Your Name <axel.burke@gmail.com>
2020-07-29 23:57:05 +0200

    Added function. Display can be toggled now

--- script.js
+++ script.js
@@ -6,8 +6,22 @@ const divCentral = document.getElementById("central-container")
 buttonInLine.addEventListener("click",displayInLine)
 buttonBlock.addEventListener("click",displayBlock)
 
-function displayInLine(){}
-function displayBlock(){}
+function displayInLine(){
+    console.log("Set inline-block")
+    divLeft.classList.add("main-containers-inline-block")
+    divLeft.classList.add("left-container-left")
+    divCentral.classList.add("main-containers-inline-block")    
+}
+
+function displayBlock(){
+    console.log("Reset to block")
+    divLeft.classList.toggle("main-containers-inline-block")
+    divLeft.classList.toggle("left-container-left")
+    divCentral.classList.toggle("main-containers-inline-block") 
+}
+
+
+
 //To do: Find a way to modify a css element properties
 
 
